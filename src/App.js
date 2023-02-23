e05d4c5ff9c7ad6ef29b7689e1860ea83e1d3c01
milleniya21 <milleniyasajy21@gmail.com>
2023-02-23 15:19:55 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import{BrowserRouter as Router,Routes,Route} from "react-router-dom";
+ import Header from './header/header';
+import Sidebar from './pages/Sidebar/sidebar';
+import Dashboard from './pages/Dashboard/dashboard'; 
+import Rfq from './pages/RFQ/rfq';
+import Purchase from './pages/Purchase orders/purchase';
+import Suppliers from './pages/Suppliers/suppliers';
+import User from './pages/Users/users';
+import Documents from './pages/Documents/documents';
+import Reports from './pages/Reports/reports';
 function App() {
   return (
+    <>
+    <Header/>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            
+      <Router>
+      <Sidebar/>
+        <Routes>
+       
+          <Route path='/dashboard'element={<Dashboard/>}></Route>
+          <Route path='/rfq'element={<Rfq/>}></Route>
+          <Route path='/purchase_orders'element={<Purchase/>}></Route>
+           <Route path='/suppliers'element={<Suppliers/>}></Route>
+          <Route path='/users'element={<User/>}></Route>
+          <Route path='/documents'element={<Documents/>}></Route>
+          <Route path='/reports'element={<Reports/>}></Route> 
+        </Routes>
+      </Router>
     </div>
+    </>
   );
-}
 
+        }
 export default App;
